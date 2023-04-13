de6379ffb924708fdb43f6d11f80ed3bb1281cea
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2023-04-13 11:43:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Breeze Magezon Builder
+
+## Installation
+
+```bash
+composer require swissup/module-breeze-magezon-builder
+bin/magento setup:upgrade --safe-mode=1
+```
---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Swissup_BreezeMagezonBuilder', __DIR__);
